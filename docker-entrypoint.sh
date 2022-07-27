f6f11c59606a79655a76985e6e4cfab891e31bf2
_den <_den@outlook.com>
2022-07-27 23:36:55 +1000

    added certs to docker variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 set -x
+export CERTPATH="/etc/ssl/nginx.crt"
+export KEYPATH="/etc/ssl/nginx.key"
 
-cd /etc/ssl/
-openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout nginx.key -out nginx.crt -subj "/C=US/ST=Action/L=Dev/O=Dev/CN=${SERVERNAME}"
+if [ ! -f ${CERTPATH} ]; then
+    cd /etc/ssl/
+    openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout nginx.key -out nginx.crt -subj "/C=US/ST=Action/L=Dev/O=Dev/CN=${SERVERNAME}"
+fi
 
-envsubst '$SERVERNAME;$UPSTREAM;$CERT_PATH;$KEY_PATH' <  /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+if [ ! -z "${FULLCHAIN_PEM}" ]; then
+    echo "${FULLCHAIN_PEM}" > /etc/ssl/fullchain.pem
+    echo "${KEY_PEM}" > /etc/ssl/key.pem
+    export CERTPATH="/etc/ssl/fullchain.pem"
+    export KEYPATH="/etc/ssl/key.pem"
+fi
+
+envsubst '$SERVERNAME;$UPSTREAM;$CERTPATH;$KEYPATH' <  /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
